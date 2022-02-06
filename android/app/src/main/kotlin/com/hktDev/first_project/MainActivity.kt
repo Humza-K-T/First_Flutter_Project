1f4516397ac44a8babb682739a681ee773c1ee51
Humza Khawar <humzakhawar@gmail.com>
2022-02-06 21:00:56 +0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/hktDev/first_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hktDev.first_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      backgroundColor: Colors.indigo[200],
+      body: Center(
+        child: Image(
+          image: NetworkImage('https://th-thumbnailer.cdn-si-edu.com/bZAar59Bdm95b057iESytYmmAjI=/1400x1050/filters:focal(594x274:595x275)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/95/db/95db799b-fddf-4fde-91f3-77024442b92d/egypt_kitty_social.jpg'),
+
+        ),
+
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red,
+      ),
+
+    );
+  }
+}
